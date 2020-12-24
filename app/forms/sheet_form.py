babf9c34fb1405671371fb7bcd497347a9cb9a37
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-24 22:10:43 +0200

    Create a Users google sheet and get the credentials.json file from google api service project Create sheet view and template and form Add update button and once it's clicked the sheet will be updated with (user id, username , email)

--- app/forms/sheet_form.py
+++ app/forms/sheet_form.py
@@ -6,8 +6,7 @@ from app import db
 
 
 class sheet_form(FlaskForm):
-    sheet_link = TextAreaField('Sheet id', validators=[Length(min=0, max=260)])
-    submit = SubmitField('Save')
+    submit = SubmitField('Update')
 
 
 
--- app/views/sheet.py
+++ app/views/sheet.py
@@ -3,16 +3,38 @@ from app import app
 from flask import render_template,flash, redirect
 from app.forms.sheet_form import sheet_form
 from app import db
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import json
+from app.models.user import Users
+
+
+
 
 @app.route('/sheet' ,methods=['GET', 'POST'])
 def sheet():
     form = sheet_form()
+    with open('app/credentials.json')as f :
+        data = json.load(f)
+        client_email= data['client_email']
+
+    scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('app/credentials.json',scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('Users').sheet1
     if form.validate_on_submit():
-        sheet_link = form.sheet_link.data
-        save_data(sheet_link)
-    return render_template('sheet.html',form=form)
+        save_data(sheet)
+    return render_template('sheet.html',form=form,client_email =client_email)
 
 
 
-def save_data(sheet_link):
-    print(sheet_link)
+def save_data(sheet):
+    sheet.clear()
+    users = Users.query.all()
+    data = []
+    for user in users:
+        data.append([user.id,user.username,user.email])
+        print(user.username)
+    sheet.insert_row(['ID','Username','Email'],1)
+    sheet.insert_rows(data,2)
+    
